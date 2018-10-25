22ac058c80705b9de869a997b4ed0d8c3c31a9b8
Ben <ben@klemens.org>
2018-10-25 17:26:03 -0400

    No need to trim then crop

--- go.py
+++ go.py
@@ -22,6 +22,6 @@ go("rm counts; for i in `seq 1 2 " + str(2*int(Count)-2) + "`; do echo $i >> cou
 Pages=go("shuf counts | awk '{print $1 \",\" $1+1}'| tr '\n' ','|sed 's/,$//'")
 
 go("rm -f mid.pdf mid1.pdf")
-go("pdfjam --trim '0in 9.5in 5in 1in' " + "  ".join([" tmp."+i +".pdf " for i in Files]) + " -o mid1.pdf")
+go("pdfjam  " + "  ".join([" tmp."+i +".pdf " for i in Files]) + " -o mid1.pdf")
 go("pdfjam mid1.pdf \"" + Pages + "\" --no-tidy -o mid.pdf")
 go("pdfcrop --clip mid.pdf cards.pdf")
